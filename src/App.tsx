67e6b1370b185bba70f1282c22eb63cb214a2b95
Kayla Noelle Chavez <knchavez14@gmail.com>
2025-11-16 15:14:27 -0800

    edited HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <header>
+        <h1>Kayla Chavez</h1>
+        <p>Front End Developer | Shopify Engineer | Creative Tech</p>
+      </header>
+
+      <main>
+        {/*About Section */}
+        <section>
+          <h2>About Me</h2>
+          <p>I'm a front end developer focused on React, Shopify, and creating seamless user experiences. I blend my background in design with techical skills problem solving to build performant, accessible, and user friendly digital products.</p>
+        </section>
+      </main>
+
+    </div>
   )
 }
 
