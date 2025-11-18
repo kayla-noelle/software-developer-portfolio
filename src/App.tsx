119c0de06ae89316dde19a708ec85595747ecc2f
kayla-noelle <knchavez14@gmail.com>
2025-11-18 11:41:20 -0800

    added loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.tsx";
-import About from "./pages/About.tsx";
-import Resume from "./pages/Resume.tsx";
-import Contact from "./pages/Contact.tsx";
-import Navbar from "./components/Navbar.tsx";
+import "./App.css";
+import { LoadingScreen } from "./components/LoadingScreen.tsx"
+import "./index.css"
 
  function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/contact" element={<Contact />}/>
-      </Routes>
+    <LoadingScreen />
     </>
   );
 }
